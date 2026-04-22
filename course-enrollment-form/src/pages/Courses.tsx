c238767e8ce3676459cfe9c4aa4bcc3335a9e1ce
chano <chanhojin94@kakao.com>
2026-04-22 16:33:20 +0900

    feat: 기본 값은 전체에 active한 상태값 주기 사용자가 마우스를 카테고리에 올려두면 검정 계열의 밑라인 주기 클릭 시 active한 상태값 주기

--- course-enrollment-form/src/pages/Courses.tsx
+++ course-enrollment-form/src/pages/Courses.tsx
@@ -5,7 +5,7 @@ import "../css/courses.css";
 export default function Courses() {
     const [categories, setCategories] = useState(["전체", "디자인", "개발", "비즈니스", "마케팅"]);
     const [ctgImgs, setCtgImgs] = useState(["🌐", "🎨", "⚙️", "📊", "🎯"]);
-    const [selectedCategory, setSelectedCategory] = useState(null);
+    const [selectedCategory, setSelectedCategory] = useState("전체");
     const [courses, setCourses] = useState([]);
 
     useEffect(() => {
@@ -17,11 +17,19 @@ export default function Courses() {
         fetchCourses();
     }, []);
 
+    const clickCategory = (category) => {
+        setSelectedCategory(category);
+    }
+
     return (
         <div className="page">
             <div className="categories">
                 {categories.map((category, idx) => (
-                    <div className="category" key={idx}>
+                    <div 
+                        className={`category ${selectedCategory === category ? "active" : ""}`}
+                        key={idx}
+                        onClick={() => clickCategory(category)}
+                        >
                         <div className="category-img">{ctgImgs[idx]}</div>
                         <div className="category-txt">{category}</div>
                     </div>
