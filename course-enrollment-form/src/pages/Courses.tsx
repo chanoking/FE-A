2d120f2bec730ed947fcb43174cbbe0544e3401b
진찬호 <chanhojin94@kakao.com>
2026-04-23 23:05:01 +0900

    feat: 지원서 폼 작성 중

--- course-enrollment-form/src/pages/Courses.tsx
+++ course-enrollment-form/src/pages/Courses.tsx
@@ -5,110 +5,140 @@ import "../css/courses.css";
 import type { Course, CourseListResponse } from "../types/course";
 
 export default function Courses() {
-    const [categories, setCategories] = useState<string[]>([]);
-    const ctgImgs: string[] = ["🌐", "🎨", "⚙️", "📊", "🎯"];
-    const [selectedCategory, setSelectedCategory] = useState<string>("전체");
-    const [courses, setCourses] = useState<Course[]>([]);
-    const [personalActive, setPersonalActive] = useState<boolean>(true);
-    const [groupActive, setGroupActive] = useState<boolean>(false);
+  const [categories, setCategories] = useState<string[]>([]);
+  const ctgImgs: string[] = ["🌐", "🎨", "⚙️", "📊", "🎯"];
+  const [selectedCategory, setSelectedCategory] = useState<string>("전체");
+  const [courses, setCourses] = useState<Course[]>([]);
+  const [personalActive, setPersonalActive] = useState<boolean>(true);
+  const [groupActive, setGroupActive] = useState<boolean>(false);
 
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
-    const filteredCourses = useMemo(() => {
-        if (selectedCategory === "전체") return courses;
+  const filteredCourses = useMemo(() => {
+    if (selectedCategory === "전체") return courses;
 
-        return courses.filter((course) => course.category === selectedCategory);
-    }, [courses, selectedCategory]);
+    return courses.filter((course) => course.category === selectedCategory);
+  }, [courses, selectedCategory]);
 
-    const clickCategory = (category: string) => {
-        setSelectedCategory(category);
+  const clickCategory = (category: string) => {
+    setSelectedCategory(category);
+  };
+
+  useEffect(() => {
+    const fetchCourses = async () => {
+      const res = await fetch("http://localhost:5175/courses");
+      const data: CourseListResponse = await res.json();
+
+      setCategories(data.categories);
+      setCourses(data.courses);
     };
+    fetchCourses();
+  }, []);
 
-    useEffect(() => {
-        const fetchCourses = async () => {
-        const res = await fetch("http://localhost:5173/courses");
-        const data: CourseListResponse = await res.json();
-        
-        setCategories(data.categories)
-        setCourses(data.courses);
-        };
-        fetchCourses();
-    }, []);
+  const calDates = (startDate, endDate) => {
+    const diff = new Date(endDate) - new Date(startDate);
+    const diffDays = Math.floor(diff / (24 * 60 * 60 * 1000));
 
-    const calDates = (startDate, endDate) => {
-        const diff = new Date(endDate) - new Date(startDate);
-        const diffDays = Math.floor(diff / (24 * 60 * 60 *1000));
+    return diffDays;
+  };
 
-        return diffDays;
-    }
-
-    return (
-        <div className="page">
-        <div className="categories">
-            {categories.map((category, idx) => (
+  return (
+    <div className="page">
+      <div className="categories">
+        {categories.map((category, idx) => (
+          <div
+            className={`category ${
+              selectedCategory === category ? "active" : ""
+            }`}
+            key={idx}
+            onClick={() => {
+              clickCategory(category);
+            }}
+          >
+            <div className="category-img">{ctgImgs[idx]}</div>
             <div
-                className={`category ${
+              className={`category-txt ${
                 selectedCategory === category ? "active" : ""
-                }`}
-                key={idx}
-                onClick={() => {
-                clickCategory(category);
-                }}
+              }`}
             >
-                <div className="category-img">{ctgImgs[idx]}</div>
-                <div className={`category-txt ${selectedCategory === category ? "active" : ""}`}>
-                    {category}
-                </div>
+              {category}
             </div>
-            ))}
-        </div>
-        <div className="lectures">
-            {filteredCourses.map((course, idx) => (
-            <div className = "course" key={idx}>
-                <div className="title">{course.title}</div>
-                <div className="description">{course.description}</div>
-                <div className="instructor">{course.instructor}</div>
-                <div className="price">\{course.price.toLocaleString()}</div>
-                <div className="status">
-                    <span className="key">최대수용 인원:</span>
-                    <span className="value"> {course.maxCapacity}</span>
-                    <span className="key"> 현재 등록:</span>
-                    <span className="value"> {course.currentEnrollment}</span>
-                </div>
-                <div className="date-scope">
-                    <span>등록기간: </span>
-                    <span>{course.startDate.split("T")[0]}~{course.endDate.split("T")[0]}</span>
-                </div>
-                <div className="hover-box">
-                    <h3>{course.title}</h3>
-                    <p style={{lineHeight: 1.2}}>{course.description}</p>
-                    <div style={{fontSize: 16}}>남은 자리: {course.maxCapacity - course.currentEnrollment}</div>
-                    <div style={{fontSize: 16}}>남은 일: {calDates(course.startDate, course.endDate)}</div>
-                    <div style={{fontSize: 16}}>가격: \{course.price.toLocaleString()}</div>
-                    <div className="option-group">
-                        <button className={`option ${(personalActive && !groupActive) ? "personal-active" : ""}`} onClick={() => {
-                            if(personalActive) return;
-                            
-                            setPersonalActive((prev) => !prev);
-                            setGroupActive(false);
-                        
-                        }}>개인 선택</button>
-                        <button className={`option ${(groupActive && !personalActive) ? "group-active" : ""}`} onClick={() => {
-                            if(groupActive) return;
-                            
-                            setGroupActive((prev) => !prev);
-                            setPersonalActive(false);
-                            }}>단체 선택</button>
-                    </div>
-                    <button 
-                        className="register-btn"
-                        onClick={() => {
-                            personalActive ? navigate("/enrollment-personal", {state: {course}}) : navigate("/enrollment-group")
-                        }}>수강신청</button>
-                </div>
+          </div>
+        ))}
+      </div>
+      <div className="lectures">
+        {filteredCourses.map((course, idx) => (
+          <div className="course" key={idx}>
+            <div className="title">{course.title}</div>
+            <div className="description">{course.description}</div>
+            <div className="instructor">{course.instructor}</div>
+            <div className="price">₩{course.price.toLocaleString()}</div>
+            <div className="status">
+              <span className="key">최대수용 인원:</span>
+              <span className="value"> {course.maxCapacity}</span>
+              <span className="key"> 현재 등록:</span>
+              <span className="value"> {course.currentEnrollment}</span>
             </div>
-            ))}
-        </div>
-        </div>
-    );
+            <div className="date-scope">
+              <span>등록기간: </span>
+              <span>
+                {course.startDate.split("T")[0]}~{course.endDate.split("T")[0]}
+              </span>
+            </div>
+            <div className="hover-box">
+              <h3>{course.title}</h3>
+              <p style={{ lineHeight: 1.2 }}>{course.description}</p>
+              <div style={{ fontSize: 16 }}>
+                남은 자리: {course.maxCapacity - course.currentEnrollment}
+              </div>
+              <div style={{ fontSize: 16 }}>
+                남은 일: {calDates(course.startDate, course.endDate)}
+              </div>
+              <div style={{ fontSize: 16 }}>
+                가격: ₩{course.price.toLocaleString()}
+              </div>
+              <div className="option-group">
+                <button
+                  className={`option ${
+                    personalActive && !groupActive ? "personal-active" : ""
+                  }`}
+                  onClick={() => {
+                    if (personalActive) return;
+
+                    setPersonalActive((prev) => !prev);
+                    setGroupActive(false);
+                  }}
+                >
+                  개인 선택
+                </button>
+                <button
+                  className={`option ${
+                    groupActive && !personalActive ? "group-active" : ""
+                  }`}
+                  onClick={() => {
+                    if (groupActive) return;
+
+                    setGroupActive((prev) => !prev);
+                    setPersonalActive(false);
+                  }}
+                >
+                  단체 선택
+                </button>
+              </div>
+              <button
+                className="register-btn"
+                onClick={() => {
+                  personalActive
+                    ? navigate("/enrollment-personal", { state: { course } })
+                    : navigate("/enrollment-group");
+                }}
+              >
+                수강신청
+              </button>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
--- course-enrollment-form/src/pages/PersonalRegistrationForm.tsx
+++ course-enrollment-form/src/pages/PersonalRegistrationForm.tsx
@@ -1,15 +1,54 @@
-import {useState, useEffect} from "react";
-import {useLocation, useNavigate} from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
+import "../css/enrollment.css";
+import type { Course } from "../types/course";
 
-export default function PersonalRegistrationForm(){
-    const location = useLocation();
-    console.log(location)
-    const {course} = location.state;
-    return (
-        <div className="page">
-            <div className="enrollment-header">
-                <h2>course.title</h2>
-            </div>
+export default function PersonalRegistrationForm() {
+  const location = useLocation();
+  const navigate = useNavigate();
+  const { course } = (location.state as { course?: Course }) ?? {};
+
+  if (!course) {
+    return <div>잘못된 접근입니다.</div>;
+  }
+
+  return (
+    <div className="enrollment-page">
+      <div className="enrollment-header">
+        <h2 className="enrollment-title">{course.title}</h2>
+      </div>
+
+      <div className="apply-layout">
+        <div className="apply-header">
+          <div
+            className="apply-header-back"
+            onClick={() => navigate("/courses")}
+          >
+            <span>〈</span>
+            <span className="apply-header-back-text">이전으로</span>
+          </div>
+
+          <div className="apply-header-bottom">
+            <h1 className="apply-heading">지원서 작성</h1>
+          </div>
         </div>
-    )
+
+        <div className="apply-form">
+          <div className="form-field">
+            <div className="form-label-row">
+              <div className="form-label">이름</div>
+              <div className="form-required">·</div>
+            </div>
+
+            <div className="form-input-wrapper">
+              <input
+                type="text"
+                className="form-input"
+                placeholder="내용을 입력해주세요."
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
